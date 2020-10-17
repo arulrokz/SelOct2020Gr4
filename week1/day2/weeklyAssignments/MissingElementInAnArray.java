45bcf8394c6f580ded24b39b353e38f624aec135
Arulraj Dhinakararajan <arulraj2005@gmail.com>
2020-10-17 14:15:41 +0530

    Add files via upload

--- week1/day2/weeklyAssignments/MissingElementInAnArray.java
+++ week1/day2/weeklyAssignments/MissingElementInAnArray.java
@@ -9,13 +9,14 @@ public class MissingElementInAnArray {
 		int[] arr = {1,2,3,4,7,6,8};
 		Arrays.sort(arr);
 		System.out.println();
-		for(int i=0;i<=arr.length-1;i++ ) {
-			for(int j=1;j<=arr.length-1;j++) {
-			
-			if(arr[i]!=arr[j]) 
-			System.out.print(j+","); 
+		for(int j=0;j<arr.length-1;j++) {
+			int i=j+1;
+			if(arr[j]!=i) {
+				System.out.print(i); 
+				break;
 			}
-		
+
+
 		}
 	}
 
