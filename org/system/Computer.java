2efed325482251884a279a09b546cc26438e9b89
Arulraj Dhinakararajan <arulraj2005@gmail.com>
2020-10-25 07:22:52 +0530

    Add files via upload

---
+++ org/system/Computer.java
@@ -0,0 +1,14 @@
+package org.system;
+
+public class Computer {
+
+	public void computerModel() {
+		System.out.println("I am a DeskTop");
+	}
+
+	public static void main(String[] args) {
+		Computer comp = new Computer();
+		comp.computerModel();
+	}
+
+}
---
+++ org/system/Desktop.java
@@ -0,0 +1,20 @@
+package org.system;
+
+public class Desktop extends Computer {
+
+	public static void main(String[] args) {
+
+		Desktop pc =new Desktop();
+		pc.desktopSize();
+		pc.computerModel();
+
+
+	}
+	public void desktopSize(){
+		System.out.println("I am a 14'' DeskTop");
+
+
+
+	}
+
+}
