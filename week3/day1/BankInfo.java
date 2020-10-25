869b597ac3b9af5110b5e6f86321d73947230b50
Arulraj Dhinakararajan <arulraj2005@gmail.com>
2020-10-25 08:10:06 +0530

    Add files via upload

---
+++ week3/day1/BankInfo.java
@@ -0,0 +1,20 @@
+package week3.day1;
+
+public class BankInfo {
+	public void savings() {
+		System.out.println("Savings");
+	}
+
+	public void fixed() {
+		System.out.println("Fixed");
+	}
+
+	public void deposit() {
+		System.out.println("BankInfo Deposit");
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
