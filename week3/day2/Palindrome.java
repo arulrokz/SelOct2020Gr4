6e186352420ba6c4956e61658d8f1d19a976fa0f
Arulraj Dhinakararajan <arulraj2005@gmail.com>
2020-10-27 08:38:19 +0530

    Add files via upload

--- week3/day2/Palindrome.java
+++ week3/day2/Palindrome.java
@@ -3,25 +3,23 @@ package week3.day2;
 public class Palindrome {
 
 	public static void main(String[] args) {
-String str1 = "Malayalam";
-String str2 = str1.toLowerCase();
-int count=0;
-char[] pal = str2.toCharArray();
+		String str1 = "Malayalam";
+		String str2 = str1.toLowerCase();
+		int count = 0;
+		char[] pal = str2.toCharArray();
 
-for (int i=0;i<pal.length;i++){
-	for(int j=pal.length-1;j>0;j--){
-		if(pal[i]==pal[j]){
-			count++;
-			break;
-			}
+		for (int i = 0; i < pal.length; i++) {
+			for (int j = pal.length - 1; j > 0; j--) {
+				if (pal[i] == pal[j]) {
+					count++;
+					break;
 				}
-}
-System.out.println(count);
-if(count==pal.length)
-{
-	System.out.println("Palindrome");
-	}else{
-		System.out.println("Not a Palindrome");
-	}
+			}
+		}
+		if (count == pal.length) {
+			System.out.println("The  word "+str1+" is a Palindrome");
+		} else {
+			System.out.println("The  word "+str1+" is not a Palindrome");
+		}
 	}
 }
